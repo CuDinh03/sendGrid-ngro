ca1926cc78036db431027d20366b1754ecbf1e91
MAC CỦA CỰ <maccuacu@MacBook-Air-ca-C.local>
2023-02-27 00:45:53 +0700

    first commit

---
+++ src/main/java/com/example/sendmailcrm/SendMailCrmApplication.java
@@ -0,0 +1,13 @@
+package com.example.sendmailcrm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendMailCrmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SendMailCrmApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sendmailcrm/SendMailCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sendmailcrm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SendMailCrmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
